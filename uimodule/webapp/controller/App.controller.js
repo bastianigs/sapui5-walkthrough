b3c20358755cff714f9452c418c7a295537c1a57
Sebastian Igescu <sebastian.igescu@bearingpoint.com>
2022-09-27 14:43:39 +0300

    its a mess, but finally got ui5 serve cmd working

---
+++ uimodule/webapp/controller/App.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    /**
+ * @param {typeof sap.ui.core.mvc.Controller} BaseController 
+ */
+function(BaseController) {
+      "use strict";
+
+      return BaseController.extend("com.myorg.myUI5Walkthrough.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
